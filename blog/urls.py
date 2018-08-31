d3d44da31ae2269e4649c6dd08c0553156ff9203
pavel_dunin <pasha.dunin.94@gmail.com>
2018-08-31 15:52:55 +0600

    viewsets added

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'posts', views.PostViewSet)
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-    url(r'^$', views.api_root),
-    url(r'^api-auth/', include('rest_framework.urls')),
-    url(r'^posts/$', views.PostList.as_view(), name='post-list'),
-    url(r'^posts/(?P<pk>[0-9]+)/$', views.PostDetail.as_view(), name='post-detail'),
-    url(r'^users/$', views.UserList.as_view(), name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail')
+    url(r'^', include(router.urls))
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
--- blog/views.py
+++ blog/views.py
@@ -7,34 +7,24 @@ from .permissions import IsAuthorOrReadOnly
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework import viewsets
 
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'posts': reverse('post-list', request=request, format=format)
-    })
 
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-            IsAuthorOrReadOnly )
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
             IsAuthorOrReadOnly)
 
-class UserList(generics.ListAPIView):
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    list and detail actions
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
